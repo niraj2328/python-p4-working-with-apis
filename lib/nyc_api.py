2178b7cbc02c89e1d294d45f83dd4a1cedb4a702
Niraj <niraj.patel@flatironschool.com>
2023-02-27 06:28:06 -0500

    completed and added a potential solution to the loader

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -20,8 +20,32 @@ class GetPrograms:
 
         return programs_list
 
+# programs = GetPrograms.get_programs()
+# print(programs)
+
 programs = GetPrograms()
 programs_schools = programs.program_school()
 
 for school in set(programs_schools):
     print(school)
+
+# # You can modify the code as follows to accept a URL through an 
+# # initialize method and use a general load_json method to retrieve JSON 
+# # from any provided URL:
+
+# import requests
+
+# class JSONLoader:
+#     def __init__(self, url):
+#         self.url = url
+        
+#     def load_json(self):
+#         response = requests.get(self.url)
+#         return response.json()
+    
+# # Now you can create an instance of the JSONLoader class by passing in a URL, 
+# # and call the load_json method on the instance to retrieve the JSON data 
+# # from that URL:
+
+# json_loader = JSONLoader("http://data.cityofnewyork.us/resource/uvks-tn5n.json")
+# json_data = json_loader.load_json()
